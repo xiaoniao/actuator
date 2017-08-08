f7f25f5276c3934de5785759f058bfb5038ae5d8
xiezx <xiezc@gold-finance.com.cn>
2017-08-08 16:26:36 +0800

    m

---
+++ BootAdminClient/src/main/java/com/liuzhuang/BootAdminClientApplication.java
@@ -0,0 +1,15 @@
+package com.liuzhuang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by liuzz on 2017/8/7.
+ */
+@SpringBootApplication
+public class BootAdminClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootAdminClientApplication.class, args);
+    }
+}
